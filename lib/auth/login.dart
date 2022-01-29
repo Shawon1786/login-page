23cae514af3edc3cd624c1e3cb7bc848cb7a5c09
MD. Shawon Hosen <97594761+Shawon1786@users.noreply.github.com>
2022-01-29 20:16:46 +0600

    login page

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,5 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
+import 'register.dart';
+
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -9,9 +14,9 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
-  late Animation <double>_animation;
-  late TextEditingController _emailController =TextEditingController(text: '');
-  late TextEditingController _passwordController =TextEditingController(text: '');
+  late Animation <double> _animation;
+  late TextEditingController _emailController = TextEditingController(text: '');
+  late TextEditingController _passwordController = TextEditingController(text: '');
   bool _obsecureText=true;
 
   @override
@@ -22,96 +27,124 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     _animationController = AnimationController(vsync: this, duration: Duration(seconds: 20));
-    _animation = CurvedAnimation(parent: _animationController, curve: Curves.linear)..addListener(() {
-      setState(() {
+    _animation = CurvedAnimation(parent: _animationController, curve: Curves.linear)
+      ..addListener(() {
+        setState(() {
+
+        });
+      })..addStatusListener((animationStatus) {
+        if (animationStatus == AnimationStatus.completed){
+          _animationController.reset();
+          _animationController.forward();
+        }
+
 
       });
-    })..addStatusListener((animationStatus) {
-      if(animationStatus==AnimationStatus){
-        _animationController.reset();
-        _animationController.forward();
-      }
-
-    });
     _animationController.forward();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    Size size =MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: AppBar(
+        title: Text('MyBook'),
+        centerTitle: true,
+      ),
       body: Stack(
         children: [
           CachedNetworkImage(
-            imageUrl: "https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547_960_720.jpg",
-            placeholder: (context, url) => Image.asset("images/1.jpg"),
+            imageUrl: "https://cdn.pixabay.com/photo/2020/04/22/12/05/adventure-5077752_960_720.jpg",
+            placeholder: (context, url) => Image.asset('images/1.jpg',fit: BoxFit.fill,),
             errorWidget: (context, url, error) => Icon(Icons.error),
             height: double.infinity,
             width: double.infinity,
             fit: BoxFit.cover,
-            alignment: FractionalOffset(_animation.value,0),
+            alignment: FractionalOffset(_animation.value, 0),
           ),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 16),
             child: ListView(
               children: [
-                SizedBox(height: size.height * 0.1,),
-                Text('Login',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.lightBlue),),
-                SizedBox(height: 10,),
+                SizedBox(height: size.height *0.1),
+                Text('Login',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.blueAccent)),
+                SizedBox(height: 10),
                 RichText(
                   text: TextSpan(
                     children: [
                       TextSpan(
-                        text: 'Don\'t have an account     ',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,color: Colors.white),
-
+                        text: "don't have an account     ",style: TextStyle(fontSize: 16,
+                          fontWeight: FontWeight.bold,color: Colors.white),
                       ),
+                      TextSpan(text: '    '),
+                      TextSpan(
+                        recognizer: TapGestureRecognizer()..onTap = () =>
+                            Navigator.push(context, MaterialPageRoute(
+                                builder: (context)=>Register()
+                            )),
+                        text: 'Register',
+                        style: TextStyle(color: Colors.lightGreenAccent,fontSize: 16,fontWeight: FontWeight.bold),
+                      ),
+
                     ],
                   ),
                 ),
-                SizedBox(height: 40,),
+                SizedBox(height: 40),
                 TextFormField(
                   keyboardType: TextInputType.emailAddress,
                   controller: _emailController,
-                  style: TextStyle(color: Colors.orangeAccent),
+                  style: TextStyle(color: Colors.white),
                   decoration: InputDecoration(
-                    hintText: 'Email',
-                    hintStyle: TextStyle(color: Colors.white),
+                    hintText: "Email",
+                    hintStyle:  TextStyle(color: Colors.white),
                     enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white)
+                      borderSide: BorderSide(color: Colors.white),
                     ),
-                    errorBorder:  UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red)
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.cyan),
                     ),
-
+                    errorBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.deepOrangeAccent),
+                    ),
                   ),
+
                 ),
                 TextFormField(
                   obscureText: _obsecureText,
                   validator: (value){
-
-                    if (value!.isEmpty || value.length<7){
+                    if(value!.isEmpty || value.length<8){
                       return 'Please enter valid password';
-                    }else{
+
+                    }
+                    else{
                       return null;
                     }
-                  },
-                  style: TextStyle(color: Colors.orangeAccent),
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    hintStyle: TextStyle(color: Colors.white),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white)
-                    ),
-                    errorBorder:  UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red)
-                    ),
 
+
+                  },
+                  controller: _passwordController,
+                  style: TextStyle(color: Colors.white70),
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    hintStyle:  TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    errorBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
                   ),
+
                 ),
-                SizedBox(height: 250,),
+                SizedBox(height: 50,),
+
                 FlatButton(
                   onPressed: (){},
-                  child: Text("Submit",style: TextStyle(fontSize: 28,color: Colors.deepOrange),),
+                  child: Text('Submit',style: TextStyle(backgroundColor: Colors.indigoAccent,fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold),),
                 ),
               ],
             ),
@@ -121,5 +154,3 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
     );
   }
 }
-
-
